f2a38b755385157167f8cdf14bb5a658b1ed74a9
ludius <57571014+ludius0@users.noreply.github.com>
2020-07-13 00:22:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ import matplotlib.pyplot as plt
 
 import networks
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 path = "mnist_dataset/mnist_train.cvs"
 dataset = networks.MnistDataset(path)
 D = networks.Discriminator()
 G = networks.Generator()
 
-def generate_random(i): 
-    """
-    Generate random digit we want as representation
-    """
-    # We could pick random one from 0 to 9, but we can always generate new "same" digit
-    return torch.rand(i)
+# def generate_random_image(i): 
+#     return torch.rand(i)
+
+def generate_random_seed(i): 
+    return torch.randn(i)
 
 ### TRAIN ###
 epochs = 1
@@ -28,10 +29,10 @@ for e in range(1, epochs+1):
 
         # train D on fake/false data
         # detach() to not calculate gradients in G (for computational cost)
-        D.train(G.forward(generate_random(1)).detach(), torch.FloatTensor([0.0]))
+        D.train(G.forward(generate_random_seed(100)).detach(), torch.FloatTensor([0.0]))
 
         # train G (generator)
-        G.train(D, generate_random(1), torch.FloatTensor([1.0]))
+        G.train(D, generate_random_seed(100), torch.FloatTensor([1.0]))
 
         if (dx+1) % 20000 == 0:
             print(f"Trained on {dx+1} images")
